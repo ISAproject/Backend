dce166e7dabaa0de4f8496d176d09eb6ebddef90
JovanKatanic <jovan.katanic0101@gmail.com>
2023-11-11 18:45:19 +0100

    initial project

---
+++ ISA2023 back/ISA2023-back/src/main/java/com/example/ISA2023/back/Isa2023BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ISA2023.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isa2023BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isa2023BackApplication.class, args);
+	}
+
+}
