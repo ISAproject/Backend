512260da43ede1adc70c61868a04a70e9d38be4b
Marko Radetic <markoradetic67@gmail.com>
2023-11-18 00:37:46 +0100

    feat: Added controllers for user roles.

---
+++ ISA2023 back/ISA2023-back/src/main/java/com/example/ISA2023/back/controllers/CompanyAdminController.java
@@ -0,0 +1,11 @@
+package com.example.ISA2023.back.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/comp_admin")
+@RequiredArgsConstructor
+public class CompanyAdminController {
+}
