a366fff7c1b3d6349eea6e34332b0af325b1c97b
Marko Radetic <markoradetic67@gmail.com>
2023-11-18 00:00:50 +0100

    feat: Added JWTService and implementation.

--- ISA2023 back/ISA2023-back/src/main/java/com/example/ISA2023/back/services/JWTService.java
+++ ISA2023 back/ISA2023-back/src/main/java/com/example/ISA2023/back/services/JWTService.java
@@ -1,4 +1,11 @@
 package com.example.ISA2023.back.services;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 public interface JWTService {
+    String extractUsername(String token);
+    String generateToken(UserDetails userDetails);
+    boolean isTokenValid(String token, UserDetails userDetails);
 }
--- ISA2023 back/ISA2023-back/src/main/java/com/example/ISA2023/back/services/impl/JWTServiceImpl.java
+++ ISA2023 back/ISA2023-back/src/main/java/com/example/ISA2023/back/services/impl/JWTServiceImpl.java
@@ -1,10 +1,12 @@
 package com.example.ISA2023.back.services.impl;
 
+import com.example.ISA2023.back.services.JWTService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.security.Key;
 
@@ -12,9 +14,10 @@ import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.function.Function;
 
-public class JWTServiceImpl {
+public class JWTServiceImpl implements JWTService{
 
-    private String generateToken(UserDetails userDetails){
+
+    public String generateToken(UserDetails userDetails){
         return Jwts.builder().setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000*60*24))
@@ -39,5 +42,12 @@ public class JWTServiceImpl {
                 .setSigningKey(getSignKey())
                 .build().parseClaimsJws(token)
                 .getBody();
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+    private boolean isTokenExpired(String token){
+        return extractClaim(token, Claims::getExpiration).before(new Date());
     }
 }
