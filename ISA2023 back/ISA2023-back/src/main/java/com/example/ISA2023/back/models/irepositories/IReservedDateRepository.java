b0f3e065f9f7bbab39edae6819fdc0c51f92163f
JovanKatanic <jovan.katanic0101@gmail.com>
2024-01-30 13:34:10 +0100

    fix: locking and transactions

--- ISA2023 back/ISA2023-back/src/main/java/com/example/ISA2023/back/models/irepositories/IReservedDateRepository.java
+++ ISA2023 back/ISA2023-back/src/main/java/com/example/ISA2023/back/models/irepositories/IReservedDateRepository.java
@@ -2,7 +2,9 @@ package com.example.ISA2023.back.models.irepositories;
 
 import com.example.ISA2023.back.models.ReservedDate;
 import com.example.ISA2023.back.user.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -16,6 +18,7 @@ public interface IReservedDateRepository extends JpaRepository<ReservedDate, Lon
     void deleteById(Long reservationId);
     @Query("SELECT r FROM ReservedDate r WHERE r.CompanyId = ?1")
     public List<ReservedDate> GetByCompany(long companyId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT r FROM ReservedDate r WHERE r.CompanyId = ?1")
     List<ReservedDate> findAllByCompanyId(long companyId);
 
